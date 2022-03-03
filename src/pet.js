24655851f8424f81a1b973092c9da7913cbd5f8a
Ed Adkins <ejmadkins@gmail.com>
2022-03-03 18:17:56 +0000

    add walk functionality

--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,30 @@
+const MAXIMUM_FITNESS = 10;
+const INIT_AGE = 0;
+const INIT_HUNGER = 0;
+
 function Pet(name) {
   this.name = name;
-  this.age = 0;
-  this.hunger = 0;
-  this.fitness = 10;
+  this.age = INIT_AGE;
+  this.hunger = INIT_HUNGER;
+  this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function () {
   this.age += 1;
   this.hunger += 5;
   this.fitness -= 3;
+};
+
+Pet.prototype.walk = function () {
+  this.fitness += 4;
+};
+
+Pet.prototype.walk = function () {
+  if (this.fitness + 4 <= MAXIMUM_FITNESS) {
+    this.fitness += 4;
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
 };
 
 module.exports = Pet;
